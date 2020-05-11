bfeb9e06a684125a7b13c458c632f97edd10bb75
CptZeru <zerulegend@gmail.com>
2020-05-11 17:31:37 +0700

    deploy build

---
+++ build/precache-manifest.44db3c209a1f3e7f823c73743497c113.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "66c2ffdf6896867aa81d278c30bf56c2",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0d0911a7c94368c1c063",
+    "url": "/static/css/main.be70f313.chunk.css"
+  },
+  {
+    "revision": "ff7d45275e8967cd0e5c",
+    "url": "/static/js/2.19fa59f4.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.19fa59f4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0d0911a7c94368c1c063",
+    "url": "/static/js/main.9abfc994.chunk.js"
+  },
+  {
+    "revision": "de674056be76f6436f38",
+    "url": "/static/js/runtime-main.5acdab21.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
